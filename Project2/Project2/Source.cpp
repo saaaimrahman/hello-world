39ec8ec78498f9c93713fdb3a13c15af9797c12c
unknown <Administrator@BTI-LRODRIGUEZ.nb.org>
2018-08-22 13:09:42 -0400

    Made changes to code

---
+++ Project2/Project2/Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string name
+	cout << "Hello";
+		cout << "what is your name";
+		cin >> name;
+		cout << "hello" << name;
+		system("PAUSE"");
+}
